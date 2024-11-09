599aa0eaac6d07b3c71e8e8696ee58298ca12e83
Harshit Soni <harshit11664@gmail.com>
2024-11-10 01:43:16 +0530

    9-Nov-2024

---
+++ Section-2 Basics- Variables, Data Types, Operators & Functions/basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,12 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add() {
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, "")
+}
+
+addBtn.addEventListener("click", add);
+
+
+// outputResult(currentResult, currentDescription);
